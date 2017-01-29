2392b7682a106c32fde7c8f352b8bcbb1ec68be2
Tanasis Vlachopulos <tanas@tanas.eu>
2017-01-29 19:50:41 +0100

    Create device status in device detail

---
+++ Presentation/WebUi/main/static/main/js/updateDeviceStatus.js
@@ -0,0 +1,22 @@
+function updateDeviceStatus(apiUrl) {
+
+    $.getJSON(apiUrl, function (result) {
+        if (!$.isEmptyObject(result)) {
+            console.log(result);
+
+            var resultTime = new Date(result['_time'] * 1000);
+            // var formatedTime = resultTime.getHours() + ':' + ('0' + resultTime.getMinutes())
+            var formatedTime = resultTime.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+
+            $('#rssi').text(result['rssi']);
+            $('#last-echo').text(formatedTime);
+            $('#ip').text(result['ip']);
+            $('#voltage').text(result['voltage']);
+        }
+        else {
+            console.log("empty telemetry");
+        }
+    });
+
+    setTimeout(updateDeviceStatus, 30000, apiUrl)
+}
